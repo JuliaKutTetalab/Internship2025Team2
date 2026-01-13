f2c8061c361f60ee36dab681f26707b04c6dbcfc
OksanaaStets <oksanastets12@gmaol.com>
2026-01-13 19:59:16 +0200

    Переписала Navigation

--- app/src/main/java/com/example/growbox/navigation/GrowBoxNavHost.kt
+++ app/src/main/java/com/example/growbox/navigation/GrowBoxNavHost.kt
@@ -1,14 +1,7 @@
 package com.example.growbox.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
